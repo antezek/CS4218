95c6aff5df86478a44309d6319ed4b85de778df9
antezek <anthonyc@127-70.priv23.nus.edu.sg>
2014-02-04 09:24:07 +0800

    Adding ctrl-z function to Shell

--- src/sg/edu/nus/comp/cs4218/impl/Shell.java
+++ src/sg/edu/nus/comp/cs4218/impl/Shell.java
@@ -1,6 +1,7 @@
 package sg.edu.nus.comp.cs4218.impl;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import sg.edu.nus.comp.cs4218.IShell;
@@ -24,12 +25,11 @@ import sg.edu.nus.comp.cs4218.impl.fileutils.PWDTool;
 import sg.edu.nus.comp.cs4218.impl.fileutils.PWDToolRunnable;
 
 /**
- * The Shell is used to interpret and execute user's
- * commands. Following sequence explains how a basic
- * shell can be implemented in Java
+ * The Shell is used to interpret and execute user's commands. Following
+ * sequence explains how a basic shell can be implemented in Java
  */
 public class Shell implements IShell {
-	//Tests
+	// Tests
 	// List of user input commands
 	private static final String CMD_PWD = "pwd";
 	private static final String CMD_CD = "cd";
@@ -41,35 +41,36 @@ public class Shell implements IShell {
 	private static final String CMD_ECHO = "echo";
 	private static final String CMD_GREP = "grep";
 	private static final String CMD_PIPE = "pipe";
-	
+
 	// Regex
 	private static final String REGEX_WHITE_SPACE = "\\s+";
-	
+
 	// List of commands
 	private enum COMMAND {
 		PWD, CD, LS, COPY, MOVE, DELETE, CAT, ECHO, GREP, PIPE, INVALID
 	}
-	
+
 	// Scanner
 	static Scanner scanner = new Scanner(System.in);
-	
+
 	// Others
 	private static String stdin;
+	private static Thread thread = null;
+	private static Thread thread2 = null;
+	private static HashMap hm = null;
 
 	@Override
 	public ITool parse(String commandline) {
-/*		if(commandline.startsWith("pwd")){
-			return new PWDTool();
-		} else {
-			//TODO Implement all other tools
-			System.err.println("Cannot parse " + commandline);
-			return null;
-		}*/
-		
+		/*
+		 * if(commandline.startsWith("pwd")){ return new PWDTool(); } else {
+		 * //TODO Implement all other tools System.err.println("Cannot parse " +
+		 * commandline); return null; }
+		 */
+
 		String userCommand = getCommand(commandline);
 		COMMAND command = determineCommandType(userCommand);
-		
-		switch(command) {
+
+		switch (command) {
 
 		case PWD:
 			return new PWDTool();
@@ -89,9 +90,9 @@ public class Shell implements IShell {
 			return new ECHOTool();
 		case GREP:
 			return new GREPTool();
-			//TODO glen: uncomment when pipe implemented
-//		case PIPE:
-//			return new PIPETool();
+			// TODO glen: uncomment when pipe implemented
+			// case PIPE:
+			// return new PIPETool();
 		default:
 			System.err.println("Cannot parse " + commandline);
 			return null;
@@ -102,81 +103,125 @@ public class Shell implements IShell {
 	public Runnable execute(ITool tool) {
 		// TODO Implement
 		File workingDir = new File(System.getProperty("user.dir"));
-		
+
 		if (tool instanceof PWDTool) {
 			return new PWDToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof CDTool) {
+		} else if (tool instanceof CDTool) {
 			return new CDToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof LSTool) {
+		} else if (tool instanceof LSTool) {
 			return new LSToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof COPYTool) {
+		} else if (tool instanceof COPYTool) {
 			return new COPYToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof MOVETool) {
+		} else if (tool instanceof MOVETool) {
 			return new MOVEToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof DELETETool) {
+		} else if (tool instanceof DELETETool) {
 			return new DELETEToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof CATTool) {
+		} else if (tool instanceof CATTool) {
 			return new CATToolRunnable(workingDir, stdin);
-		}
-		else if (tool instanceof ECHOTool) {
+		} else if (tool instanceof ECHOTool) {
 			return new ECHOToolRunnable(workingDir, stdin);
 		}
-		//TODO glen: uncomment when grep/pipe implemented
-//		else if (tool instanceof GREPTool) {
-//			return new GREPToolRunnable(workingDir, stdin);
-//		}
-//		else if (tool instanceof PIPETool) {
-//			return new PIPEToolRunnable(workingDir, stdin);	
-//		}
-		
+		// TODO glen: uncomment when grep/pipe implemented
+		// else if (tool instanceof GREPTool) {
+		// return new GREPToolRunnable(workingDir, stdin);
+		// }
+		// else if (tool instanceof PIPETool) {
+		// return new PIPEToolRunnable(workingDir, stdin);
+		// }
 		return null;
 	}
 
 	@Override
 	public void stop(Runnable toolExecution) {
-		//TODO Implement
+		// TODO Implement
 	}
-	
+
 	/**
-	 * Do Forever
-     * 1. Wait for a user input
-     * 2. Parse the user input. Separate the command and its arguments
-     * 3. Create a new thread to execute the command
-     * 4. Execute the command and its arguments on the newly created thread. Exit with the status code of the executed command
-     * 5. In the shell, wait for the thread to complete execution
-     * 6. Report the exit status of the command to the user
+	 * Do Forever 1. Wait for a user input 2. Parse the user input. Separate the
+	 * command and its arguments 3. Create a new thread to execute the command
+	 * 4. Execute the command and its arguments on the newly created thread.
+	 * Exit with the status code of the executed command 5. In the shell, wait
+	 * for the thread to complete execution 6. Report the exit status of the
+	 * command to the user
 	 */
 	public static void main(String[] args) {
-		//TODO Implement
-		try {
-			while (true) {
-				stdin = readUserInput();
-				Shell s = new Shell();
-				ITool tool = s.parse(stdin);
-				Runnable r = s.execute(tool);
+		startRun();
+	}
 
-				Thread thread = new Thread(r);
-				thread.start();
-				thread.join();
-			}
+	public static void startRun() {
+		Runnable r = null;
+		Runnable r2 = null;
+		hm = new HashMap();
+		try {
+			do {
+				stdin = readUserInput(0);
+				if (!stdin.equals("")) {
+					Shell s = new Shell();
+					ITool tool = s.parse(stdin);
+					r = s.execute(tool);
+					thread = new Thread(r); // Spawn a thread to execute the
+											// command
+					hm.put("t1", thread);
+					thread.start();
+					r2 = new Runnable() {
+						Scanner eofScan;
+
+						public void run() {
+							if (thread.isAlive()) { // Check if thread is still
+													// processing. if yes, give
+													// option to break.
+								readUserInput(1);
+							}
+						}
+					};
+					thread2 = new Thread(r2); // Spawn a thread to listen for
+												// break command.
+					thread2.sleep(500); // Delay for result to show first before
+										// cmd prompt
+					thread2.start();
+					while (true) {
+						Thread.currentThread().sleep(500);
+						if (thread.isAlive() == false) {
+							thread2.stop();
+							startRun();
+							break;
+						}
+					}
+				} else {
+					break; // stdin is EMPTY!
+				}
+			} while (stdin.equalsIgnoreCase("exit"));
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			System.out.println("Thread interrupted"); // Interrupted
 		}
 	}
 
-	private static String readUserInput() {
-		System.out.print("command:");
-		return scanner.nextLine();
+	public static String readUserInput(int type) {
+		String s = "";
+		if (type == 0) {
+			System.out.print("command:");
+		} else {
+			System.out.print("Type \"ctrl-z\" to break: ");
+		}
+		try {
+			scanner = new Scanner(System.in);
+			s = scanner.nextLine();
+			if (s.equalsIgnoreCase("ctrl-z")) {
+				thread = (Thread) hm.get("t1");
+				thread.stop();
+			} else if (type == 1 && !(s.equalsIgnoreCase("ctrl-z"))) {
+				System.out.println("Invalid command");
+				readUserInput(1);
+			}
+		} catch (NullPointerException e) {
+			System.out.println("System Exit");
+			System.exit(0);
+		}
+		return s;
 	}
 
 	private static COMMAND determineCommandType(String command) {
-		
+
 		if (command.equalsIgnoreCase(CMD_PWD)) {
 			return COMMAND.PWD;
 		} else if (command.equalsIgnoreCase(CMD_CD)) {
@@ -194,14 +239,14 @@ public class Shell implements IShell {
 		} else if (command.equalsIgnoreCase(CMD_ECHO)) {
 			return COMMAND.ECHO;
 		} else if (command.equalsIgnoreCase(CMD_GREP)) {
-				return COMMAND.GREP;
+			return COMMAND.GREP;
 		} else if (command.equalsIgnoreCase(CMD_PIPE)) {
 			return COMMAND.PIPE;
 		} else {
 			return COMMAND.INVALID;
 		}
 	}
-	
+
 	private static String getCommand(String userInputString) {
 		return userInputString.trim().split(REGEX_WHITE_SPACE)[0];
 	}
